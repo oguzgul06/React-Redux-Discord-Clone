a4faac085a93ce885664a9fb93f1f26e4d6a9503
oguz gül <ogzgl95@outlook.com>
2021-08-19 11:03:41 +0300

    SidebarVoice side added to Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -3,6 +3,9 @@ import "./Sidebar.css";
 import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import AddIcon from "@material-ui/icons/Add";
 import SidebarChannel from "./subcomponents/SidebarChannel";
+import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
+import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
+import CallIcon from "@material-ui/icons/Call";
 
 function Sidebar() {
   return (
@@ -20,11 +23,28 @@ function Sidebar() {
           </div>
           <AddIcon className="sidebar__addChannel" />
         </div>
+
         <div className="sidebar__channelsList">
           <SidebarChannel />
           <SidebarChannel />
           <SidebarChannel />
           <SidebarChannel />
+        </div>
+      </div>
+
+      <div className="sidebar__voice">
+        <SignalCellularAltIcon
+          className="sidebar__voiceIcon"
+          fontSize="large"
+        />
+        <div className="sidebar__voiceInfo">
+          <h3>Voice Connected</h3>
+          <p>Stream</p>
+        </div>
+
+        <div className="sidebar__voiceIcons">
+          <InfoOutlinedIcon />
+          <CallIcon />
         </div>
       </div>
     </div>
