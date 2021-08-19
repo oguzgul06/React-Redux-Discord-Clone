be17bd6cdd4e70b0f114f9ad7bd8b1b95b067e63
oguz gül <ogzgl95@outlook.com>
2021-08-19 12:21:49 +0300

    SidebarProfile side added to Sidebar component

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -6,6 +6,10 @@ import SidebarChannel from "./subcomponents/SidebarChannel";
 import SignalCellularAltIcon from "@material-ui/icons/SignalCellularAlt";
 import InfoOutlinedIcon from "@material-ui/icons/InfoOutlined";
 import CallIcon from "@material-ui/icons/Call";
+import { Avatar } from "@material-ui/core";
+import MicIcon from "@material-ui/icons/Mic";
+import HeadsetIcon from "@material-ui/icons/Headset";
+import SettingsIcon from "@material-ui/icons/Settings";
 
 function Sidebar() {
   return (
@@ -45,6 +49,20 @@ function Sidebar() {
         <div className="sidebar__voiceIcons">
           <InfoOutlinedIcon />
           <CallIcon />
+        </div>
+      </div>
+
+      <div className="sidebar__profile">
+        <Avatar src="https://avatars.githubusercontent.com/u/72518895?s=400&u=dc226eb23f1bf0b03fcf851f628b8886b5365f42&v=4" />
+        <div className="sidebar__profileInfo">
+          <h3>@ooguzzggull</h3>
+          <p>#thisIsMyID</p>
+        </div>
+
+        <div className="sidebar__profileIcons">
+          <MicIcon />
+          <HeadsetIcon />
+          <SettingsIcon />
         </div>
       </div>
     </div>
