d3c55f12ba9a76e21f6cc4849071cb1e0145acb1
oguz gül <ogzgl95@outlook.com>
2021-08-19 16:37:33 +0300

    Build copmlate login and added to dispatch tool on app.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,8 +1,26 @@
 import React from "react";
 import "./Login.css";
+import { Button } from "@material-ui/core";
+import { auth, provider } from "../firebase";
 
 function Login() {
-  return <div className="login"></div>;
+  const signIn = (event) => {
+      auth.signInWithPopup(provider)
+      .catch((error) => alert(error.message))
+  };
+
+  return (
+    <div className="login">
+      <div className="login__logo">
+        <img
+          src="https://global-uploads.webflow.com/5e157548d6f7910beea4e2d6/60415153fea4767867265233_Microinfluencers%201140%20wide-64.png"
+          alt=""
+        />
+      </div>
+
+      <Button onClick={signIn}>Sign In</Button>
+    </div>
+  );
 }
 
 export default Login;
