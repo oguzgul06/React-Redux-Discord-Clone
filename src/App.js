b2114628d14949665b09782b28aca57a827d0719
oguz gül <ogzgl95@outlook.com>
2021-08-19 10:13:47 +0300

    Create Sidebar component and build complate sidebar-top side and sidebar-channels side

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from "react";
 import "./App.css";
+import Sidebar from "./components/Sidebar";
 
 function App() {
   return (
     <div className="app">
-      <h1>Let's Build Discord Clone 🚀</h1>
-
       {/* Sidebar */}
-
+      <Sidebar />
       {/* Chat */}
     </div>
   );
--- src/features/Counter.js
+++ src/features/Counter.js
@@ -7,7 +7,7 @@ import {
   incrementAsync,
   incrementIfOdd,
   selectCount,
-} from './counterSlice';
+} from '../app/features/counterSlice';
 import styles from './Counter.css';
 
 export function Counter() {
