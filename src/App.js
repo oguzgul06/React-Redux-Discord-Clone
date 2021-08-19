df0759e3e3e8b692462723ff9c6fcd35cdaac590
oguz gül <ogzgl95@outlook.com>
2021-08-19 13:19:27 +0300

    Create Chat component and ChatHeader subComponent

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React from "react";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
+import Chat from "./components/Chat";
 
 function App() {
   return (
     <div className="app">
       {/* Sidebar */}
       <Sidebar />
+
       {/* Chat */}
+      <Chat />
     </div>
   );
 }
