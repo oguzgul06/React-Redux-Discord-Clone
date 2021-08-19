739744b778b298524651d47ca25048103fdf99ae
oguz gül <ogzgl95@outlook.com>
2021-08-19 15:09:53 +0300

    Redux tool added to App component and create Login component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Sidebar from "./components/Sidebar";
 import Chat from "./components/Chat";
+import { selectUser } from "./features/userSlice";
+import Login from "./components/Login";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className="app">
-      {/* Sidebar */}
-      <Sidebar />
+      {user ? (
+        <>
+          {/* Sidebar */}
+          <Sidebar />
 
-      {/* Chat */}
-      <Chat />
+          {/* Chat */}
+          <Chat />
+        </>
+      ) : (
+        <Login />
+      )}
     </div>
   );
 }
